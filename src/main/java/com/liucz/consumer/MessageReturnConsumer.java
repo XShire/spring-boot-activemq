3d5b2fd7d198b56c3c26402c1a1445072c9a3cab
liucz <liu227world@163.com>
2018-04-26 17:55:58 +0800

    添加消费者返回值和返回值队列

--- src/main/java/com/liucz/consumer/MessageReturnConsumer.java
+++ src/main/java/com/liucz/consumer/MessageReturnConsumer.java
@@ -3,20 +3,17 @@ package com.liucz.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Consumer1{
+public class MessageReturnConsumer {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     // 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
-    @JmsListener(destination = "message.queue")
-    @SendTo("out.queue")
-    public String receiveQueue(String text) {
-        logger.info("发送消息:"+text);
-        return "return message"+text;
+    @JmsListener(destination = "out.queue")
+    public void receiveQueue(String text) {
+        logger.info("发送消息返回值:"+text);
     }
 
 }
