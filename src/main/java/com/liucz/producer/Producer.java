17b94ddad1a29577a7bcaa453624e771cbdd9cfc
liucz <liu227world@163.com>
2018-04-26 14:02:57 +0800

    上传ActiveMQDemo，使用内存方式

---
+++ src/main/java/com/liucz/producer/Producer.java
@@ -0,0 +1,20 @@
+package com.liucz.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.jms.Destination;
+
+@Service
+public class Producer {
+
+    @Autowired
+    private JmsMessagingTemplate jmsTemplate;
+
+    // 发送消息，destination是发送到的队列，message是待发送的消息
+    public void sendMessage(Destination destination, final Object message) {
+        jmsTemplate.convertAndSend(destination, message);
+    }
+
+}
